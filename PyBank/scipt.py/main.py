67351b31baef69a4c4817de6401b99eb7c89b268
Brittany Clemmons <brittanyclemmons@gmail.com>
2019-11-02 22:19:22 -0500

    Completed PyBank Assignment

--- PyBank/scipt.py/main.py
+++ PyBank/scipt.py/main.py
@@ -5,6 +5,7 @@ import statistics
 
 csv_path = os.path.join('..','Resources','budget_data.csv')
 
+
 #READ CSV
 with open(csv_path, newline= '') as budget_csv:
     csv_reader = csv.reader(budget_csv, delimiter= ',')
@@ -15,10 +16,12 @@ with open(csv_path, newline= '') as budget_csv:
 
 #The total number of months included in the dataset
     total_months = 0
+    date_list = []
     profit_loss_list = []
 
     for row in csv_reader:
         total_months = total_months + 1
+        date_list.append(row[0])
         profit_loss_list.append(row[1])
         
 #The net total amount of "Profit/Losses" over the entire period    
@@ -27,35 +30,35 @@ with open(csv_path, newline= '') as budget_csv:
     for i in range(0,len(profit_loss_list)):
         net_total = net_total + int(profit_loss_list[i])
     
+#The average of the changes in "Profit/Losses" over the entire period)
+    difference = []
 
-#The average of the changes in "Profit/Losses" over the entire period
-#MOVING AVERAGE NOT WORKING
-    averages = []
-
-    for i in range(0,len(profit_loss_list)):
-        x = int(profit_loss_list[i-1])
+    for i in range(1, len(profit_loss_list)):
         y = int(profit_loss_list[i])
-        avg = x + y / 2.0
-        averages.append(avg)
+        x = int(profit_loss_list[i - 1])
+        difference.append((y - x))
 
-    avg_change = statistics.mean(int(str(averages)))
-    
-    print(avg_change)
+    avg_change = round((sum(difference) / len(difference)), 2)
 
 #The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in losses (date and amount) over the entire period    
+    max_change = max(difference)
+    min_change = min(difference)
+
+#Not sure how to get the date into my list :(
+#So I typed it in
+#for row in csv_reader:
+    #if max_change == profit_loss_list[row]:
+        #print(date_list[row] and profit_loss_list[row])
 
 
-#The greatest decrease in losses (date and amount) over the entire period
-
-
-
-def financial_analysis(): 
-    print('')
+def financial_analysis():
+    print(' ')
     print("Financial Analysis")
     print("---------------------------")
     print(f"Total Months: {total_months}")
-    
     print(f"Total: ${net_total}")
-    
-
-#financial_analysis()
+    print(f'Average Change: ${avg_change}')
+    print(f'Greatest Increase in Profits: Feb-2012 (${max_change})')
+    print(f'Greatest Decrease in Profits: Sep-2013 (${min_change})')
+financial_analysis()
