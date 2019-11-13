cd492f1ad8e61b4a1725a68678f8bfd896b3f247
Brittany Clemmons <brittanyclemmons@gmail.com>
2019-11-12 20:23:23 -0600

    PyPoll initiation

---
+++ PyPoll/main.py
@@ -0,0 +1,13 @@
+#IMPORT CSV
+
+
+
+
+
+
+
+#Total number of votes cast
+#Complete list of Candidates who received votes
+#Percentage of votes each candidate won
+#Total number of votes each candidate won
+#Winner of the election based on popular vote
