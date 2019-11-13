1890cfcff851aab6141ccbebdbcc6de21e5fc3b9
Brittany Clemmons <brittanyclemmons@gmail.com>
2019-11-13 02:00:03 -0600

    Completed PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,70 +8,75 @@ csv_path = os.path.join('Resources', 'election_data.csv')
 with open(csv_path, newline = '') as election_csv:
     csv_reader = csv.reader(election_csv, delimiter = ',')
     csv_header = next(csv_reader)
-    print(csv_header)
+    #print(csv_header)
+    print('Calculating winner...')
+
 
 #IMPORT DATA
-
     voter_id = []
-    county = []
     candidates = []
 
     for row in csv_reader:
         voter_id.append(row[0])
-        county.append(row[1])
         candidates.append(row[2])
     
 
-#Total number of votes cast
-
+#TOTAL NUMBER OF VOTES CAST
 total_votes = len(voter_id)
 #print(total_votes)
 
 
-#Complete list of Candidates who received votes
+#COMPLETE LIST OF CANDIDATE WHO RECEIVED VOTES
+find_candidates= set(candidates)
+unique_candidates = list(find_candidates)
+sorted_candidates = sorted(unique_candidates)
+#print(sorted_candidates)
 
-#unique_candidates = np.array(candidates)
-#print(np.unique(unique_candidates))
 
-#Total number of votes each candidate won
-
-
+#TOTAL NUMBER OF VOTES EACH CANDIDATE WON
 Correy_Count = 0
 Khan_Count = 0
 Li_Count = 0
 O_Tooley_Count = 0
 
-for i in range(0, len(candidates)):
-    if candidates[i] == "Correy":
-        Correy_Count = Correy_Count + 1
-    elif candidates[i] == "Khan":
-        Khan_Count = Khan_Count + 1
-    elif candidates[i] == "Li":
-        Li_Count = Li_Count + 1
-    elif candidates[i] == "O'Tooley":
-        O_Tooley_Count = O_Tooley_Count + 1
-    else:
-        print("Unknown candidate found")
-        print("Run unique candidate function")
-
-#print(Khan_Count)    
+Correy_Count = candidates.count("Correy")
+Khan_Count = candidates.count("Khan")
+Li_Count = candidates.count("Li")
+O_Tooley_Count = candidates.count("O'Tooley")
 #print(Correy_Count)
+#print(Khan_Count)
 #print(Li_Count)
 #print(O_Tooley_Count)
 
-#Percentage of votes each candidate won
 
-Khan_win = round(((Khan_Count / total_votes) * 100), 3)
+#PERCENTAGE OF VOTES EACH CANDIDATE WON
 Correy_win = round(((Correy_Count / total_votes) * 100), 3)
+Khan_win = round(((Khan_Count / total_votes) * 100), 3)
 Li_win = round(((Li_Count / total_votes) * 100), 3)
 OTooley_win = round(((O_Tooley_Count / total_votes) * 100), 3)
-#print(Khan_win)
+
 #print(Correy_win)
+#print(Khan_win)
 #print(Li_win)
 #print(OTooley_win)
 
-#Winner of the election based on popular vote
 
+#WINNER OF THE ELECTION BASED ON POPULAR VOTE
+percentage = []
+
+percentage.append(Correy_win)
+percentage.append(Khan_win)
+percentage.append(Li_win)
+percentage.append(OTooley_win)
+
+max = max(percentage)
+index = percentage.index(max)
+
+winner = sorted_candidates[index]
+#print(winner)
+
+
+#PRINT RESULT TO TERMINAL
 def election_results():
     print(" ")
     print("Election Results")
@@ -83,7 +88,24 @@ def election_results():
     print(f'Li:       {Li_win}%  ({Li_Count})')
     print(f"O'Tooley:  {OTooley_win}%  ({O_Tooley_Count})")
     print("--------------------------")
-    print(f'Winner: Khan')
+    print(f'Winner: {winner}')
     print("--------------------------")
-
 election_results()
+
+
+#OUTPUT TO TXT FILE
+def election_results_txt():
+    e_res = 'election_results'
+    print(" ", file=open(e_res, 'w'))
+    print("Election Results", file=open(e_res, 'a'))
+    print("--------------------------", file=open(e_res, 'a'))
+    print(f'Total Votes: {total_votes}', file=open(e_res, 'a'))
+    print("--------------------------", file=open(e_res, 'a'))
+    print(f'Khan:     {Khan_win}%  ({Khan_Count})', file=open(e_res, 'a'))
+    print(f'Correy:   {Correy_win}%  ({Correy_Count})', file=open(e_res, 'a'))
+    print(f'Li:       {Li_win}%  ({Li_Count})', file=open(e_res, 'a'))
+    print(f"O'Tooley:  {OTooley_win}%  ({O_Tooley_Count})", file=open(e_res, 'a'))
+    print("--------------------------", file=open(e_res, 'a'))
+    print(f'Winner: {winner}', file=open(e_res, 'a'))
+    print("--------------------------", file=open(e_res, 'a'))
+election_results_txt()
